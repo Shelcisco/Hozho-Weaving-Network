349dff1ff21568bd84d30dcf2359a4ad1a5a84ed
blayne-04 <befuller2004@gmail.com>
2023-06-13 18:19:15 -0700

    lowered file size limit (should still be fine)

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ const server = new ApolloServer({
 });
 const app = express();
 
-app.use(express.urlencoded({ limit: "3mb", extended: false }));
-app.use(express.json({limit: "3mb"}));
+app.use(express.urlencoded({ limit: "1mb", extended: false }));
+app.use(express.json({limit: "1mb"}));
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
